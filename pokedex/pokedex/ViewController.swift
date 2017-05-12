dd2dd04b1ba4b5ae803d5b65fd246c68d933f59c
shawnkoon <shawn9405@gmail.com>
2017-05-11 22:56:39 -0700

    Table list view added along with few list of pokemons and 2nd view controller page added

--- pokedex/pokedex/ViewController.swift
+++ pokedex/pokedex/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var pokedexTableView: UITableView!
+    
+    var pokemons = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pokedexTableView.dataSource = self
+        pokedexTableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = pokemons[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
